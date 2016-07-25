9eec1153601606b7bc0bfe05c8b1ce409d0eb9de
RomanYshko <kinokadru21@gmail.com>
2016-07-25 14:11:47 +0300

    shop

---
+++ shop-test/config/db_params.php
@@ -0,0 +1,9 @@
+<?php
+
+// Массив с параметрами подключения к базе данных
+return array(
+    'host' => 'localhost',
+    'dbname' => 'j95617oe_p',
+    'user' => 'j95617oe_p',
+    'password' => '661212p',
+);
